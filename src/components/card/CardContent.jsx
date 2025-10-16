a960988121d9748f8997a73ffce6482d756b89d8
khansa-020 <khansaaraein@gmail.com>
2025-10-16 16:39:09 +0500

    commit

--- src/components/card/CardContent.jsx
+++ src/components/card/CardContent.jsx
@@ -4,12 +4,12 @@ export default function CardContent() {
   return (
     <div className="flex item-center gap-[20px] ml-[10px] mb-[48px]">
       <div className="">
-        <img src="/images/card/Copy Icon B.svg" alt="line" className='w-[1px] h-[195px]' />
+        <img src="/images/card/WS Chrome Line.svg" alt="line" className='w-[1px] h-[195px]' />
       </div>
 
       <div className="flex flex-col font-[12px] tracking-[1px] ml-[10px] leading-none">
         <div className="flex items-center gap-[43px]">
-          <img src="icons/Group 34927.svg" alt="BBC" className="h-[30px]" />
+          <img src="images/card/logo/bbc-log.png" alt="BBC" className="h-[30px]" />
           <a 
             href="https://www.bbc.co.uk/news/articles/c8jm2xlk1gdo" 
             target="_blank" 
@@ -40,7 +40,6 @@ export default function CardContent() {
           <img src="/images/card/Open in New Window.svg" alt="open window" className='h-[22px] cursor-pointer' />
           <img src="/images/card/Copy Icon B.svg" alt="copy" className='h-[22px] cursor-pointer' />
           <img src="/images/card/Share Icon B.svg" alt="share" className='h-[22px] cursor-pointer' />
-          <img src="/images/card/Delete Icon B.svg" alt="delete" className='h-[22px] cursor-pointer' />
         </div>
       </div>
     </div>
